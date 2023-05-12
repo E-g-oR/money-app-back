d707469f9fbe25d32aedc9cfac4eedf339218052
Egor Muzychkin <e.myzichkin@rit.va>
2023-05-12 12:46:12 +0300

    add global validation pipes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from 'src/dto/auth';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
-  login() {
+  login(@Body() body: AuthDto) {
     return this.authService.login();
   }
 
